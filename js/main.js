94bc111b20bd21e2d4941945e85d79152a6b5743
kpcyrd <git@rxv.cc>
2014-09-12 16:00:41 +0200

    Remove duplicate code

--- js/main.js
+++ js/main.js
@@ -5,34 +5,34 @@ var MAX_RSS = 5;
 
 $(document).ready(function() {
     var i = 0;
+
+    var addItem = function(title, link) {
+        if(i >= MAX_RSS) return;
+
+        var item = $('<a>')
+            .attr('class', 'css-truncate')
+            .attr('href', link)
+            .attr('target', '_blank')
+            .text(title);
+        $('#news').append($('<li>').append(item));
+
+        i++;
+    };
+
     rss.forEach(function(feed) {
         $.get('/feeds/' + feed + '.rss', function(data) {
             $(data).find('item').forEach(function(entry) {
-                if(i >= MAX_RSS) return;
                 var title = $(entry).find('title').text();
                 var link = $(entry).find('link').text();
 
-                var item = $('<a>')
-                    .attr('class', 'css-truncate')
-                    .attr('href', link)
-                    .attr('target', '_blank')
-                    .text(title);
-                $('#news').append($('<li>').append(item));
-                i++;
+                addItem(title, link);
             });
 
             $(data).find('entry').forEach(function(entry) {
-                if(i >= MAX_RSS) return;
                 var title = $(entry).find('title').text();
                 var link = $(entry).find('link').attr('href');
 
-                var item = $('<a>')
-                    .attr('class', 'css-truncate')
-                    .attr('href', link)
-                    .attr('target', '_blank')
-                    .text(title);
-                $('#news').append($('<li>').append(item));
-                i++;
+                addItem(title, link);
             });
         });
     });
